b7e817f0787421511c791833398f2462b8451feb
Maxim Smakouz <m.smakouz@gmail.com>
2021-12-07 12:39:41 +0200

    Delete UUID

--- src/Common/Schema/RegistryModifier.php
+++ src/Common/Schema/RegistryModifier.php
@@ -7,7 +7,6 @@ namespace Cycle\ORM\Entity\Macros\Common\Schema;
 use Cycle\Database\Schema\AbstractColumn;
 use Cycle\Database\Schema\AbstractTable;
 use Cycle\ORM\Entity\Macros\Exception\MacroCompilationException;
-use Cycle\ORM\Entity\Macros\Uuid\UuidTypecast;
 use Cycle\Schema\Definition\Field;
 use Cycle\Schema\Definition\Map\FieldMap;
 use Cycle\Schema\Registry;
@@ -78,30 +77,6 @@ class RegistryModifier
 
         return $this->table->column($columnName)->type(self::STRING_COLUMN);
     }
-
-    /** @throws MacroCompilationException */
-    public function addUuidColumn(string $columnName, string $fieldName): AbstractColumn
-    {
-        if ($this->fields->has($fieldName)) {
-            if (!$this->isType(self::UUID_COLUMN, $fieldName, $columnName)) {
-                throw new MacroCompilationException(sprintf('Field %s must be of type uuid.', $fieldName));
-            }
-            $this->validateColumnName($fieldName, $columnName);
-            if ($this->fields->get($fieldName)->getTypecast() === null) {
-                $this->fields->get($fieldName)->setTypecast([UuidTypecast::class, 'cast']);
-            }
-
-            return $this->table->column($columnName);
-        }
-
-        $this->fields->set(
-            $fieldName,
-            (new Field())->setColumn($columnName)->setType('uuid')->setTypecast([UuidTypecast::class, 'cast'])
-        );
-
-        return $this->table->column($columnName)->type(self::UUID_COLUMN);
-    }
-
 
     public function findColumnName(string $fieldName, ?string $columnName): ?string
     {
